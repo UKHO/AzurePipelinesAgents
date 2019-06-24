554af6a05a1254a49932af3199b7b251a1aea16a
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2019-06-24 15:01:18 +0100

    Changes to VM setup
    - removes public IP from the process
    - removes port 22
    - use of array object for servernames

--- modules/azdo_ubuntuagent/main.tf
+++ modules/azdo_ubuntuagent/main.tf
@@ -1,39 +1,14 @@
-resource "azurerm_network_security_group" "port22" {
-  name                = "port22"
-  location            = "${var.AZURERM_RESOURCE_GROUP_MAIN_LOCATION}"
-  resource_group_name = "${var.AZURERM_RESOURCE_GROUP_MAIN_NAME}"
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_public_ip" "VM" {
-  name                = "${var.PREFIX}-${var.VM}-ip"
-  location            = "${var.AZURERM_RESOURCE_GROUP_MAIN_LOCATION}"
-  resource_group_name = "${var.AZURERM_RESOURCE_GROUP_MAIN_NAME}"
-  allocation_method   = "Dynamic"
-}
 
 resource "azurerm_network_interface" "VM" {
   name                      = "${var.PREFIX}-${var.VM}-nic"
   location                  = "${var.AZURERM_RESOURCE_GROUP_MAIN_LOCATION}"
   resource_group_name       = "${var.AZURERM_RESOURCE_GROUP_MAIN_NAME}"
-  network_security_group_id = "${azurerm_network_security_group.port22.id}"
+  network_security_group_id = "${var.AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID}"
 
   ip_configuration {
     name                          = "configuration"
     subnet_id                     = "${var.AZURERM_SUBNET_ID}"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = "${azurerm_public_ip.VM.id}"
   }
 }
 
