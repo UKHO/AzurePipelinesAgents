6fe2cf0b00027f9e479ac5dbe30c70d9d8bc2b15
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2019-06-25 12:27:17 +0100

    adds windows server 2019 to the setup

---
+++ modules/azdo_ws2019agent/main.tf
@@ -0,0 +1,57 @@
+
+resource "azurerm_network_interface" "VM" {
+  name                      = "${var.PREFIX}-${var.VM}-nic"
+  location                  = "${var.AZURERM_RESOURCE_GROUP_MAIN_LOCATION}"
+  resource_group_name       = "${var.AZURERM_RESOURCE_GROUP_MAIN_NAME}"
+  network_security_group_id = "${var.AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID}"
+
+  ip_configuration {
+    name                          = "configuration"
+    subnet_id                     = "${var.AZURERM_SUBNET_ID}"
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "VM" {
+  name                  = "${var.PREFIX}-${var.VM}"
+  location              = "${var.AZURERM_RESOURCE_GROUP_MAIN_LOCATION}"
+  resource_group_name   = "${var.AZURERM_RESOURCE_GROUP_MAIN_NAME}"
+  network_interface_ids = ["${azurerm_network_interface.VM.id}"]
+  vm_size               = "Standard_D2s_v3"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "WindowsServer"
+    sku       = "2019-DataCenter-Core"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "${var.PREFIX}-${var.VM}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "${var.VM}"
+    admin_username = "${var.ADMIN_USERNAME}"
+    admin_password = "${var.ADMIN_PASSWORD}"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      path     = "${var.ADMIN_SSHKEYPATH}"
+      key_data = "${var.ADMIN_SSHKEYDATA}"
+    }
+  }
+
+  tags = {
+    environment = "agent"
+  }
+}
---
+++ modules/azdo_ws2019agent/variables.tf
@@ -0,0 +1,53 @@
+variable "AZURE_REGION" {
+  type = "string"
+}
+
+variable "PREFIX" {
+  type = "string"
+}
+variable "VSTS_ACCOUNT" {
+  type = "string"
+}
+variable "VSTS_TOKEN" {
+  type = "string"
+}
+variable "VSTS_POOL" {
+  type = "string"
+}
+variable "VM" {
+  type = "string"
+}
+variable "ADMIN_USERNAME" {
+  type = "string"
+}
+variable "ADMIN_PASSWORD" {
+  type = "string"
+}
+
+variable "ADMIN_SSHKEYPATH" {
+  type = "string"
+}
+
+variable "ADMIN_SSHKEYDATA" {
+  type = "string"
+}
+
+variable "AZURERM_RESOURCE_GROUP_MAIN_NAME" {
+  type = "string"
+}
+
+variable "AZURERM_RESOURCE_GROUP_MAIN_LOCATION" {
+  type = "string"
+}
+
+variable "AZURERM_VIRTUAL_NETWORK_MAIN_NAME" {
+  type = "string"
+}
+
+variable "AZURERM_SUBNET_ID" {
+  type = "string"
+}
+
+variable "AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID" {
+  type = "string"
+}
