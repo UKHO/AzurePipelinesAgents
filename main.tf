0efc8e1459c45c686cbf93b79b8ce7a4145fa18b
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2019-06-27 09:12:52 +0100

    removing a machine for a limiting issue

--- main.tf
+++ main.tf
@@ -47,26 +47,6 @@ module "pool_agent1" {
   AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID = "${azurerm_network_security_group.main.id}"
   VM                                     = "${element(var.SERVERNAMES, 0)}"
 }
-
-module "pool_agent2" {
-  source                                 = "./modules/azdo_ubuntuagent"
-  PREFIX                                 = "${var.PREFIX}"
-  VSTS_POOL                              = "${var.VSTS_POOL}"
-  VSTS_ACCOUNT                           = "${var.VSTS_ACCOUNT}"
-  VSTS_TOKEN                             = "${var.VSTS_TOKEN}"
-  ADMIN_USERNAME                         = "${var.ADMIN_USERNAME}"
-  ADMIN_PASSWORD                         = "${var.ADMIN_PASSWORD}"
-  ADMIN_SSHKEYPATH                       = "${var.ADMIN_SSHKEYPATH}"
-  ADMIN_SSHKEYDATA                       = "${var.ADMIN_SSHKEYDATA}"
-  AZURE_REGION                           = "${var.AZURE_REGION}"
-  AZURERM_RESOURCE_GROUP_MAIN_NAME       = "${azurerm_resource_group.main.name}"
-  AZURERM_RESOURCE_GROUP_MAIN_LOCATION   = "${azurerm_resource_group.main.location}"
-  AZURERM_VIRTUAL_NETWORK_MAIN_NAME      = "${azurerm_virtual_network.main.name}"
-  AZURERM_SUBNET_ID                      = "${azurerm_subnet.main.id}"
-  AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID = "${azurerm_network_security_group.main.id}"
-  VM                                     = "${element(var.SERVERNAMES, 1)}"
-}
-
 module "pool_agent3" {
   source                                 = "./modules/azdo_ws2019agent"
   PREFIX                                 = "${var.PREFIX}"
