b5d5e37ee1cadc553fce574b5ea0a098547deb1b
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2019-07-10 10:52:57 +0100

    what is submitted

--- tfplan.sh
+++ tfplan.sh
@@ -1,12 +1,16 @@
-export TF_VAR_PREFIX=$TF_VAR_PREFIX-$(date +%F)
-export TF_VAR_VSTS_TOKEN=$(az keyvault secret show --vault-name "$1" --name "$2-vsts-token"  --query value -o tsv)
-export ARM_CLIENT_ID=$(az keyvault secret show --vault-name "$1" --name "$2-clientid"  --query value -o tsv)
-export ARM_CLIENT_SECRET=$(az keyvault secret show --vault-name "$1" --name "$2-clientsecret"  --query value -o tsv)
-export ARM_SUBSCRIPTION_ID=$(az keyvault secret show --vault-name "$1" --name "$2-subscriptionid"  --query value -o tsv)
-export ARM_TENANT_ID=$(az keyvault secret show --vault-name "$1" --name "$2-tenantid"  --query value -o tsv)
+echo $1
 
-echo "Execute Terraform plan"
-terraform plan -out=tfplan -input=false
+echo $2
 
-echo "Execute Terraform apply"
-terraform apply "tfplan"
+#export TF_VAR_PREFIX=$TF_VAR_PREFIX-$(date +%F)
+#export TF_VAR_VSTS_TOKEN=$(az keyvault secret show --vault-name "$1" --name "$2-vsts-token"  --query value -o tsv)
+#export ARM_CLIENT_ID=$(az keyvault secret show --vault-name "$1" --name "$2-clientid"  --query value -o tsv)
+#export ARM_CLIENT_SECRET=$(az keyvault secret show --vault-name "$1" --name "$2-clientsecret"  --query value -o tsv)
+#export ARM_SUBSCRIPTION_ID=$(az keyvault secret show --vault-name "$1" --name "$2-subscriptionid"  --query value -o tsv)
+#export ARM_TENANT_ID=$(az keyvault secret show --vault-name "$1" --name "$2-tenantid"  --query value -o tsv)
+
+#echo "Execute Terraform plan"
+#terraform plan -out=tfplan -input=false
+#
+#echo "Execute Terraform apply"
+#terraform apply "tfplan"
