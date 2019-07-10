24ae1179dbb9920da36438d1dcb56fafedd3ed60
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2019-07-10 10:55:00 +0100

    just the token

--- tfplan.sh
+++ tfplan.sh
@@ -1,9 +1,6 @@
-echo $1
+export TF_VAR_PREFIX=$TF_VAR_PREFIX-$(date +%F)
+export TF_VAR_VSTS_TOKEN=$(az keyvault secret show --vault-name "$1" --name "$2-vsts-token"  --query value -o tsv)
 
-echo $2
-
-#export TF_VAR_PREFIX=$TF_VAR_PREFIX-$(date +%F)
-#export TF_VAR_VSTS_TOKEN=$(az keyvault secret show --vault-name "$1" --name "$2-vsts-token"  --query value -o tsv)
 #export ARM_CLIENT_ID=$(az keyvault secret show --vault-name "$1" --name "$2-clientid"  --query value -o tsv)
 #export ARM_CLIENT_SECRET=$(az keyvault secret show --vault-name "$1" --name "$2-clientsecret"  --query value -o tsv)
 #export ARM_SUBSCRIPTION_ID=$(az keyvault secret show --vault-name "$1" --name "$2-subscriptionid"  --query value -o tsv)
