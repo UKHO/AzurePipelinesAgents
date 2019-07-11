485139b1b2ea8de4699ef62641dbb18ff14ba5ea
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2019-07-11 12:37:41 +0100

    Using keyvault for some secret store (#16)
    * Using Azure key vault for secrets
      - SPN is used to access keyvault, remember to give access to keyvaults.
    * using arguments for tfplan sh script
    * rename of plan step

--- providers.tf
+++ providers.tf
@@ -1,7 +1,3 @@
 provider "azurerm" {
   version         = "~> 1.21"
-  client_id       = "${var.AZURE_CLIENT_ID}"
-  subscription_id = "${var.AZURE_SUBSCRIPTION_ID}"
-  client_secret   = "${var.AZURE_CLIENT_SECRET}"
-  tenant_id       = "${var.AZURE_TENANT_ID}"
 }
--- variables.tf
+++ variables.tf
@@ -1,16 +1,3 @@
-variable "AZURE_CLIENT_ID" {
-  type = "string"
-}
-variable "AZURE_SUBSCRIPTION_ID" {
-  type = "string"
-}
-variable "AZURE_CLIENT_SECRET" {
-  type = "string"
-}
-variable "AZURE_TENANT_ID" {
-  type = "string"
-}
-
 variable "AZURE_REGION" {
   type = "string"
 }
