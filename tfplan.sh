c0153d95a699bccd1de004d895aafcdbbd96e195
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2019-08-02 14:26:38 +0100

    use of makefile to run behaviour

--- tfplan.sh
+++ tfplan.sh
@@ -1,15 +1,25 @@
+#! /bin/bash
+
+export vaultName="AzDOLive-keyvault"
+export keyPrefix="terraform-azdolive"
 export TF_VAR_PREFIX=$TF_VAR_PREFIX-$(date +%F)
-export TF_VAR_VSTS_TOKEN=$(az keyvault secret show --vault-name "$1" --name "$2-vsts-token"  --query value -o tsv)
 
-export ARM_CLIENT_ID=$(az keyvault secret show --vault-name "$1" --name "$2-clientid"  --query value -o tsv)
-export ARM_CLIENT_SECRET=$(az keyvault secret show --vault-name "$1" --name "$2-clientsecret"  --query value -o tsv)
-export ARM_SUBSCRIPTION_ID=$(az keyvault secret show --vault-name "$1" --name "$2-subscriptionid"  --query value -o tsv)
-export ARM_TENANT_ID=$(az keyvault secret show --vault-name "$1" --name "$2-tenantid"  --query value -o tsv)
+export ARM_CLIENT_ID=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-clientid"  --query value -o tsv)
+export ARM_CLIENT_SECRET=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-clientsecret"  --query value -o tsv)
+export ARM_SUBSCRIPTION_ID=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-subscriptionid"  --query value -o tsv)
+export ARM_TENANT_ID=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-tenantid"  --query value -o tsv)
+export ARM_ACCESS_KEY=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-accesskey"  --query value -o tsv)
 
-terraform init
+chmod 755 ./secret
+
+
+make init
 
 echo "Execute Terraform plan"
-terraform plan -out=tfplan -input=false
+#terraform plan -out=tfplan -input=false
+make plan
 
 echo "Execute Terraform apply"
-terraform apply "tfplan"
+#terraform apply "tfplan"
+make apply
+
