86fb0c48eb52c2dbc28e61702f2f2622989317cc
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2019-08-05 08:26:46 +0100

    comment around chmod

--- tfplan.sh
+++ tfplan.sh
@@ -10,13 +10,13 @@ export ARM_SUBSCRIPTION_ID=$(az keyvault secret show --vault-name "$vaultName" -
 export ARM_TENANT_ID=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-tenantid"  --query value -o tsv)
 export ARM_ACCESS_KEY=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-accesskey"  --query value -o tsv)
 
+# ./secret is a bash script, but does not get execution rights when recieved by the build agent.
 chmod 755 ./secret
 
 
 make init
 
 echo "Execute Terraform plan"
-#terraform plan -out=tfplan -input=false
 make plan
 
 echo "Execute Terraform apply"
