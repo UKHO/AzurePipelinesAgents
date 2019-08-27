c7a2387671b8a93e21c650391797c71e4be29368
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2019-08-27 11:35:32 +0100

    moving secrets

--- tfplan.sh
+++ tfplan.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-export vaultName="AzDOLive-keyvault"
-export keyPrefix="terraform-azdolive"
+export vaultName="AzDOLive-KV"
+export keyPrefix="terraform"
 export TF_VAR_RUN_DATE=$(date +%F)
 
-export ARM_CLIENT_ID=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-clientid"  --query value -o tsv)
-export ARM_CLIENT_SECRET=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-clientsecret"  --query value -o tsv)
-export ARM_SUBSCRIPTION_ID=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-subscriptionid"  --query value -o tsv)
-export ARM_TENANT_ID=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-tenantid"  --query value -o tsv)
-export ARM_ACCESS_KEY=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-accesskey"  --query value -o tsv)
+export ARM_CLIENT_ID=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-client-id"  --query value -o tsv)
+export ARM_CLIENT_SECRET=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-client-secret"  --query value -o tsv)
+export ARM_SUBSCRIPTION_ID=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-subscription-id"  --query value -o tsv)
+export ARM_TENANT_ID=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-tenant-id"  --query value -o tsv)
+export ARM_ACCESS_KEY=$(az keyvault secret show --vault-name "$vaultName" --name "$keyPrefix-access-key"  --query value -o tsv)
 
 # ./secret is a bash script, but does not get execution rights when recieved by the build agent.
 chmod 755 ./secret
