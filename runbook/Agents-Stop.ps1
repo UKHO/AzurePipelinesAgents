669d20b3d72c8891dca669a82cac7cbe1db7a8d8
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2019-09-13 14:19:58 +0100

    Create Agents-Stop.ps1

---
+++ runbook/Agents-Stop.ps1
@@ -0,0 +1,52 @@
+param(
+    [Parameter(Mandatory)]
+    $Numbers,
+    $VmNamePattern
+)
+
+$numberArray = $numbers.Split(' ')
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+#Get all ARM resources from all resource groups
+$ResourceGroups = Get-AzureRmResourceGroup | Where ResourceGroupName -like $VmNamePattern
+
+foreach ($ResourceGroup in $ResourceGroups)
+{    
+    $ResourceGroupName = $ResourceGroup.ResourceGroupName
+    Write-Output ("Showing resources in resource group " + $ResourceGroupName)
+    $Resources = Get-AzureRmVM -ResourceGroupName $ResourceGroupName 
+    
+    ForEach ($vm in $Resources)
+    {
+        $vmName = $vm.Name
+        $vmNumber = $vmName.Substring($vmName.length-2)
+        Write-Output("$vmName : $vmNumber")
+        if($numberArray -contains $vmNumber) {
+            Write-Output ($vmName)
+            $vm | Stop-AzureRmVM -Force
+        }
+    }
+    Write-Output ("")
+} 
