1520e138048fcbd46ebaee720a1f1ac4ef961174
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2019-12-17 15:56:53 +0000

    String interpolation fix

--- terraform/modules/azdo_ws2019agent/variables.tf
+++ terraform/modules/azdo_ws2019agent/variables.tf
@@ -1,55 +1,55 @@
 variable "AZURE_REGION" {
-  type = "string"
+  type = string
 }
 
 variable "VSTS_ACCOUNT" {
-  type = "string"
+  type = string
 }
 variable "VSTS_TOKEN" {
-  type = "string"
+  type = string
 }
 variable "VSTS_POOL_PREFIX" {
-  type = "string"
+  type = string
 }
 variable "VM" {
-  type = "string"
+  type = string
 }
 variable "ADMIN_USERNAME" {
-  type = "string"
+  type = string
 }
 variable "ADMIN_PASSWORD" {
-  type = "string"
+  type = string
 }
 
 variable "ADMIN_SSHKEYPATH" {
-  type = "string"
+  type = string
 }
 
 variable "ADMIN_SSHKEYDATA" {
-  type = "string"
+  type = string
 }
 
 variable "AZURERM_RESOURCE_GROUP_MAIN_NAME" {
-  type = "string"
+  type = string
 }
 
 variable "AZURERM_RESOURCE_GROUP_MAIN_LOCATION" {
-  type = "string"
+  type = string
 }
 
 variable "AZURERM_VIRTUAL_NETWORK_MAIN_NAME" {
-  type = "string"
+  type = string
 }
 
 variable "AZURERM_SUBNET_ID" {
-  type = "string"
+  type = string
 }
 
 variable "AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID" {
-  type = "string"
+  type = string
 }
 variable "BRANCH" {
-  type = "string"
+  type = string
 }
 variable "VSTS_AGENT_COUNT" {
   type        = number
@@ -57,7 +57,7 @@ variable "VSTS_AGENT_COUNT" {
   default     = 3
 }
 variable "TAGS" {
-  type = "map"
+  type = map
 }
 
 variable "vm_name" {
