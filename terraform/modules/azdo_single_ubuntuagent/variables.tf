a01060ab99aa272beb5c003e560ca1f5396808f5
Marc Denman <marc.denman@ukho.gov.uk>
2020-01-02 09:34:42 +0000

    Add variable descriptions to single agent module

--- terraform/modules/azdo_single_ubuntuagent/variables.tf
+++ terraform/modules/azdo_single_ubuntuagent/variables.tf
@@ -3,7 +3,9 @@ variable "vm_name" {
   description = "Name of the VM to be created. Must fulfill UKHO standards"
 }
 variable "vm_size" {
-  type = string 
+  type = string
+  default     = "Standard_F4s"
+  description = "String of the VM size from https://docs.microsoft.com/en-us/azure/virtual-machines/windows/sizes"
 }
 variable "VM_RG_NAME"{
   type = string
@@ -33,16 +35,21 @@ variable "SPOKE_SUBNET_ID" {
 }
 variable "AZDO_ORGANISATION"{
   type = string
+  description = "Name of the organisation where the agents will be added to"
 }
 variable "AZDO_TOKEN"{
   type = string
+  description = "Access token with permissions to added agents to the AzDo organisation"
 }
 variable "AZDO_POOL_PREFIX"{
   type = string
   default = "UKHO"
+  description = "String to be prefixed to the pool name"
 }
 variable "BRANCH" {
   type = string
+  default     = "master"
+  description = "Allows the install script to be run from a different branch, used when testing pull requests"
 }
 variable "TAGS" {
   type = map
