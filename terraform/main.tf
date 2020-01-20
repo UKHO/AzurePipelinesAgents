d38213b4d4d0826931c8e2ad8ea6c6528e1c91ee
Marc Denman <marc.denman@ukho.gov.uk>
2020-01-20 10:36:10 +0000

    Change names to be more descriptive

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,31 @@
 terraform {
   required_version = "~> 0.12.5"
+}
 
-  #  backend "azurerm" {}
+data "azurerm_resource_group" "spoke-rg" {
+  name = "AzDoLive-RG"
+}
+
+data "azurerm_virtual_network" "spoke-vnet" {
+  name                = "AzDoLive-vnet"
+  resource_group_name = "${data.azurerm_resource_group.spoke-rg.name}"
+}
+
+data "azurerm_subnet" "spoke-vnet-subnet" {
+  name                 = "azdoagents-prd-subnet"
+  resource_group_name  = "${data.azurerm_resource_group.spoke-rg.name}"
+  virtual_network_name = "${data.azurerm_virtual_network.spoke-vnet.name}"
+}
+
+data "azurerm_network_security_group" "spoke-nsg" {
+  name                = "AzDoLive-nsg"
+  resource_group_name = "${data.azurerm_resource_group.spoke-rg.name}"
 }
 
 resource "azurerm_resource_group" "main" {
   name     = "m-${var.VSTS_ACCOUNT}-agents-${var.ENVIRONMENT}-${var.RUN_DATE}-rg"
   location = var.AZURE_REGION
   tags     = merge(var.TAGS, { "ACCOUNT" = "${var.VSTS_ACCOUNT}", "RUN_DATE" = "${var.RUN_DATE}" })
-}
-
-data "azurerm_resource_group" "data" {
-  name = "AzDoLive-RG"
-}
-
-data "azurerm_virtual_network" "data" {
-  name                = "AzDoLive-vnet"
-  resource_group_name = "${data.azurerm_resource_group.data.name}"
-}
-
-data "azurerm_subnet" "data" {
-  name                 = "azdoagents-prd-subnet"
-  resource_group_name  = "${data.azurerm_resource_group.data.name}"
-  virtual_network_name = "${data.azurerm_virtual_network.data.name}"
-}
-
-data "azurerm_network_security_group" "data" {
-  name                = "AzDoLive-nsg"
-  resource_group_name = "${data.azurerm_resource_group.data.name}"
 }
 
 module "pool_agent1-ubuntu" {
@@ -42,9 +40,9 @@ module "pool_agent1-ubuntu" {
   AZURE_REGION                           = var.AZURE_REGION
   AZURERM_RESOURCE_GROUP_MAIN_NAME       = azurerm_resource_group.main.name
   AZURERM_RESOURCE_GROUP_MAIN_LOCATION   = azurerm_resource_group.main.location
-  AZURERM_VIRTUAL_NETWORK_MAIN_NAME      = data.azurerm_virtual_network.data.name
-  AZURERM_SUBNET_ID                      = data.azurerm_subnet.data.id
-  AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID = data.azurerm_network_security_group.data.id
+  AZURERM_VIRTUAL_NETWORK_MAIN_NAME      = data.azurerm_virtual_network.spoke-vnet.name
+  AZURERM_SUBNET_ID                      = data.azurerm_subnet.spoke-vnet-subnet.id
+  AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID = data.azurerm_network_security_group.spoke-nsg.id
   VM                                     = element(var.SERVERNAMES, 0)
   BRANCH                                 = var.BRANCH
   TAGS                                   = var.TAGS
@@ -65,9 +63,9 @@ module "pool_agent2-ubuntu" {
   AZURE_REGION                           = var.AZURE_REGION
   AZURERM_RESOURCE_GROUP_MAIN_NAME       = azurerm_resource_group.main.name
   AZURERM_RESOURCE_GROUP_MAIN_LOCATION   = azurerm_resource_group.main.location
-  AZURERM_VIRTUAL_NETWORK_MAIN_NAME      = data.azurerm_virtual_network.data.name
-  AZURERM_SUBNET_ID                      = data.azurerm_subnet.data.id
-  AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID = data.azurerm_network_security_group.data.id
+  AZURERM_VIRTUAL_NETWORK_MAIN_NAME      = data.azurerm_virtual_network.spoke-vnet.name
+  AZURERM_SUBNET_ID                      = data.azurerm_subnet.spoke-vnet-subnet.id
+  AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID = data.azurerm_network_security_group.spoke-nsg.id
   VM                                     = element(var.SERVERNAMES, 1)
   BRANCH                                 = var.BRANCH
   TAGS                                   = var.TAGS
@@ -88,9 +86,9 @@ module "pool_agent3-ubuntu" {
   AZURE_REGION                           = var.AZURE_REGION
   AZURERM_RESOURCE_GROUP_MAIN_NAME       = azurerm_resource_group.main.name
   AZURERM_RESOURCE_GROUP_MAIN_LOCATION   = azurerm_resource_group.main.location
-  AZURERM_VIRTUAL_NETWORK_MAIN_NAME      = data.azurerm_virtual_network.data.name
-  AZURERM_SUBNET_ID                      = data.azurerm_subnet.data.id
-  AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID = data.azurerm_network_security_group.data.id
+  AZURERM_VIRTUAL_NETWORK_MAIN_NAME      = data.azurerm_virtual_network.spoke-vnet.name
+  AZURERM_SUBNET_ID                      = data.azurerm_subnet.spoke-vnet-subnet.id
+  AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID = data.azurerm_network_security_group.spoke-nsg.id
   VM                                     = element(var.SERVERNAMES, 2)
   BRANCH                                 = var.BRANCH
   TAGS                                   = var.TAGS
@@ -111,9 +109,9 @@ module "pool_agent4-ws2019-vs2019" {
   AZURE_REGION                           = var.AZURE_REGION
   AZURERM_RESOURCE_GROUP_MAIN_NAME       = azurerm_resource_group.main.name
   AZURERM_RESOURCE_GROUP_MAIN_LOCATION   = azurerm_resource_group.main.location
-  AZURERM_VIRTUAL_NETWORK_MAIN_NAME      = data.azurerm_virtual_network.data.name
+  AZURERM_VIRTUAL_NETWORK_MAIN_NAME      = data.azurerm_virtual_network.spoke-vnet.name
   AZURERM_SUBNET_ID                      = data.azurerm_subnet.data.id
-  AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID = data.azurerm_network_security_group.data.id
+  AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID = data.azurerm_network_security_group.spoke-nsg.id
   VM                                     = element(var.SERVERNAMES, 3)
   BRANCH                                 = var.BRANCH
   TAGS                                   = var.TAGS
