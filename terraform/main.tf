baa1ccc36983c0de67ae24b29ffdc117b2077eae
Marc Denman <marc.denman@ukho.gov.uk>
2020-01-20 14:13:13 +0000

    Add single agent vms

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 0.12.5"
 }
 
-data "azurerm_resource_group" "data" {
+data "azurerm_resource_group" "spoke-rg" {
   name = "m-spokeconfig-RG"
 }
 
@@ -22,10 +22,14 @@ data "azurerm_network_security_group" "spoke-nsg" {
   resource_group_name = data.azurerm_resource_group.spoke-rg.name
 }
 
-resource "azurerm_resource_group" "main" {
-  name     = "m-${var.VSTS_ACCOUNT}-agents-${var.ENVIRONMENT}-${var.RUN_DATE}-rg"
+locals {
+  run_date = formatdate("YYYY-MM-DD", timestamp())
+}
+
+resource "azurerm_resource_group" "vm-rg" {
+  name     = "m-${var.VSTS_ACCOUNT}-agents-${var.ENVIRONMENT}-${local.run_date}-rg"
   location = var.AZURE_REGION
-  tags     = merge(var.TAGS, { "ACCOUNT" = "${var.VSTS_ACCOUNT}", "RUN_DATE" = "${var.RUN_DATE}" })
+  tags     = merge(var.TAGS, { "ACCOUNT" = "${var.VSTS_ACCOUNT}", "RUN_DATE" = "${local.run_date}" })
 }
 
 module "pool_agent1-ubuntu" {
@@ -38,8 +42,8 @@ module "pool_agent1-ubuntu" {
   ADMIN_SSHKEYPATH                       = var.ADMIN_SSHKEYPATH
   ADMIN_SSHKEYDATA                       = var.ADMIN_SSHKEYDATA
   AZURE_REGION                           = var.AZURE_REGION
-  AZURERM_RESOURCE_GROUP_MAIN_NAME       = azurerm_resource_group.main.name
-  AZURERM_RESOURCE_GROUP_MAIN_LOCATION   = azurerm_resource_group.main.location
+  AZURERM_RESOURCE_GROUP_MAIN_NAME       = azurerm_resource_group.vm-rg.name
+  AZURERM_RESOURCE_GROUP_MAIN_LOCATION   = azurerm_resource_group.vm-rg.location
   AZURERM_VIRTUAL_NETWORK_MAIN_NAME      = data.azurerm_virtual_network.spoke-vnet.name
   AZURERM_SUBNET_ID                      = data.azurerm_subnet.spoke-vnet-subnet.id
   AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID = data.azurerm_network_security_group.spoke-nsg.id
@@ -48,7 +52,7 @@ module "pool_agent1-ubuntu" {
   TAGS                                   = var.TAGS
   vm_name                                = "MAZDO${upper(var.ENVIRONMENT)}${element(var.SERVERNAMES, 0)}"
   vm_size                                = var.VM_SIZE
-  run_date                               = var.RUN_DATE
+  run_date                               = local.run_date
 }
 
 module "pool_agent2-ubuntu" {
@@ -61,8 +65,8 @@ module "pool_agent2-ubuntu" {
   ADMIN_SSHKEYPATH                       = var.ADMIN_SSHKEYPATH
   ADMIN_SSHKEYDATA                       = var.ADMIN_SSHKEYDATA
   AZURE_REGION                           = var.AZURE_REGION
-  AZURERM_RESOURCE_GROUP_MAIN_NAME       = azurerm_resource_group.main.name
-  AZURERM_RESOURCE_GROUP_MAIN_LOCATION   = azurerm_resource_group.main.location
+  AZURERM_RESOURCE_GROUP_MAIN_NAME       = azurerm_resource_group.vm-rg.name
+  AZURERM_RESOURCE_GROUP_MAIN_LOCATION   = azurerm_resource_group.vm-rg.location
   AZURERM_VIRTUAL_NETWORK_MAIN_NAME      = data.azurerm_virtual_network.spoke-vnet.name
   AZURERM_SUBNET_ID                      = data.azurerm_subnet.spoke-vnet-subnet.id
   AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID = data.azurerm_network_security_group.spoke-nsg.id
@@ -71,33 +75,10 @@ module "pool_agent2-ubuntu" {
   TAGS                                   = var.TAGS
   vm_name                                = "MAZDO${upper(var.ENVIRONMENT)}${element(var.SERVERNAMES, 1)}"
   vm_size                                = var.VM_SIZE
-  run_date                               = var.RUN_DATE
+  run_date                               = local.run_date
 }
 
-module "pool_agent3-ubuntu" {
-  source                                 = "./modules/azdo_ubuntuagent"
-  VSTS_POOL_PREFIX                       = var.VSTS_POOL_PREFIX
-  VSTS_ACCOUNT                           = var.VSTS_ACCOUNT
-  VSTS_TOKEN                             = var.VSTS_TOKEN
-  ADMIN_USERNAME                         = var.ADMIN_USERNAME
-  ADMIN_PASSWORD                         = var.ADMIN_PASSWORD
-  ADMIN_SSHKEYPATH                       = var.ADMIN_SSHKEYPATH
-  ADMIN_SSHKEYDATA                       = var.ADMIN_SSHKEYDATA
-  AZURE_REGION                           = var.AZURE_REGION
-  AZURERM_RESOURCE_GROUP_MAIN_NAME       = azurerm_resource_group.main.name
-  AZURERM_RESOURCE_GROUP_MAIN_LOCATION   = azurerm_resource_group.main.location
-  AZURERM_VIRTUAL_NETWORK_MAIN_NAME      = data.azurerm_virtual_network.spoke-vnet.name
-  AZURERM_SUBNET_ID                      = data.azurerm_subnet.spoke-vnet-subnet.id
-  AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID = data.azurerm_network_security_group.spoke-nsg.id
-  VM                                     = element(var.SERVERNAMES, 2)
-  BRANCH                                 = var.BRANCH
-  TAGS                                   = var.TAGS
-  vm_name                                = "MAZDO${upper(var.ENVIRONMENT)}${element(var.SERVERNAMES, 2)}"
-  vm_size                                = var.VM_SIZE
-  run_date                               = var.RUN_DATE
-}
-
-module "pool_agent4-ws2019-vs2019" {
+module "pool_agent3-ws2019-vs2019" {
   source                                 = "./modules/azdo_ws2019agent"
   VSTS_POOL_PREFIX                       = var.VSTS_POOL_PREFIX
   VSTS_ACCOUNT                           = var.VSTS_ACCOUNT
@@ -107,15 +88,55 @@ module "pool_agent4-ws2019-vs2019" {
   ADMIN_SSHKEYPATH                       = var.ADMIN_SSHKEYPATH
   ADMIN_SSHKEYDATA                       = var.ADMIN_SSHKEYDATA
   AZURE_REGION                           = var.AZURE_REGION
-  AZURERM_RESOURCE_GROUP_MAIN_NAME       = azurerm_resource_group.main.name
-  AZURERM_RESOURCE_GROUP_MAIN_LOCATION   = azurerm_resource_group.main.location
+  AZURERM_RESOURCE_GROUP_MAIN_NAME       = azurerm_resource_group.vm-rg.name
+  AZURERM_RESOURCE_GROUP_MAIN_LOCATION   = azurerm_resource_group.vm-rg.location
   AZURERM_VIRTUAL_NETWORK_MAIN_NAME      = data.azurerm_virtual_network.spoke-vnet.name
-  AZURERM_SUBNET_ID                      = data.azurerm_subnet.data.id
+  AZURERM_SUBNET_ID                      = data.azurerm_subnet.spoke-vnet-subnet.id
   AZURERM_NETWORK_SECURITY_GROUP_MAIN_ID = data.azurerm_network_security_group.spoke-nsg.id
-  VM                                     = element(var.SERVERNAMES, 3)
+  VM                                     = element(var.SERVERNAMES, 2)
   BRANCH                                 = var.BRANCH
   TAGS                                   = var.TAGS
-  vm_name                                = "MAZDO${upper(var.ENVIRONMENT)}${element(var.SERVERNAMES, 3)}"
+  vm_name                                = "MAZDO${upper(var.ENVIRONMENT)}${element(var.SERVERNAMES, 2)}"
   vm_size                                = var.VM_SIZE
-  run_date                               = var.RUN_DATE
+  run_date                               = local.run_date
+}
+
+module "ubuntu_01" {
+  source           = "./modules/azdo_single_ubuntuagent"
+  vm_name          = "MAZDO${upper(var.ENVIRONMENT)}AGT01"
+  vm_size          = var.VM_SIZE
+  VM_RG_NAME       = azurerm_resource_group.vm-rg.name
+  VM_REGION        = var.AZURE_REGION
+  ADMIN_SSHKEYPATH = var.ADMIN_SSHKEYPATH
+  ADMIN_SSHKEYDATA = var.ADMIN_SSHKEYDATA
+  ADMIN_USERNAME   = var.ADMIN_USERNAME
+
+  run_date        = local.run_date
+  SPOKE_NSG_ID    = data.azurerm_network_security_group.spoke-nsg.id
+  SPOKE_SUBNET_ID = data.azurerm_subnet.spoke-vnet-subnet.id
+
+  AZDO_ORGANISATION = var.VSTS_ACCOUNT
+  AZDO_TOKEN        = var.VSTS_TOKEN
+  BRANCH            = var.BRANCH
+  TAGS              = var.TAGS
+}
+
+module "ubuntu_02" {
+  source           = "./modules/azdo_single_ubuntuagent"
+  vm_name          = "MAZDO${upper(var.ENVIRONMENT)}AGT01"
+  vm_size          = var.VM_SIZE
+  VM_RG_NAME       = azurerm_resource_group.vm-rg.name
+  VM_REGION        = var.AZURE_REGION
+  ADMIN_SSHKEYPATH = var.ADMIN_SSHKEYPATH
+  ADMIN_SSHKEYDATA = var.ADMIN_SSHKEYDATA
+  ADMIN_USERNAME   = var.ADMIN_USERNAME
+
+  run_date        = local.run_date
+  SPOKE_NSG_ID    = data.azurerm_network_security_group.spoke-nsg.id
+  SPOKE_SUBNET_ID = data.azurerm_subnet.spoke-vnet-subnet.id
+
+  AZDO_ORGANISATION = var.VSTS_ACCOUNT
+  AZDO_TOKEN        = var.VSTS_TOKEN
+  BRANCH            = var.BRANCH
+  TAGS              = var.TAGS
 }
